a67d2193e59496c037e8c9e730d367cf96610d39
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2020-11-12 17:17:49 +0100

    Add accessor attribute and add unit tests

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,7 @@ require 'httparty'
 require 'nokogiri'
 
 class Scraper
+  attr_reader :parse_page
   def initialize
     url = 'https://www.nike.com/w/new-3n82y'
     doc = HTTParty.get(url).body
@@ -9,11 +10,11 @@ class Scraper
   end
 
   def names
-    item_container.css('.product-card_body').children.map(&:text).compact
+    item_container.css('.product-card__title').children.map(&:text).compact
   end
 
   def prices
-    item_container.css('.product-card').children.map(&:text).compact
+    item_container.css('.product-price').children.map(&:text).compact
   end
 
   private
